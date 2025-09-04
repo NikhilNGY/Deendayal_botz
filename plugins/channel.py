999b3c43d76f361dd9d3513d57341985f78fa8f6
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-05 00:29:56 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -1,5 +1,5 @@
 from pyrogram import Client, filters
-from info import CHANNELS
+from Deendayal_botz.info import CHANNELS
 from database.ia_filterdb import save_file
 
 media_filter = filters.document | filters.video | filters.audio
