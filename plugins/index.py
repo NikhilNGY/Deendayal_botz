e952c8d1a35e224251a9396354aefc0d45861612
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-05 00:26:03 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -3,7 +3,7 @@ import asyncio
 from pyrogram import Client, filters, enums
 from pyrogram.errors import FloodWait
 from pyrogram.errors.exceptions.bad_request_400 import ChannelInvalid, ChatAdminRequired, UsernameInvalid, UsernameNotModified
-from info import ADMINS, INDEX_REQ_CHANNEL as LOG_CHANNEL
+from Deendayal_botz.info import ADMINS, INDEX_REQ_CHANNEL as LOG_CHANNEL
 from database.ia_filterdb import save_file
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from utils import temp
