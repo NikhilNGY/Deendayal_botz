a18671cdb3b427ceb501bb27a7861fdc4a85274b
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-05 00:29:01 +0530

    Update connection.py

--- plugins/connection.py
+++ plugins/connection.py
@@ -1,7 +1,7 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from database.connections_mdb import add_connection, all_connections, if_active, delete_connection
-from info import ADMINS
+from Deendayal_botz.info import ADMINS
 import logging
 
 logger = logging.getLogger(__name__)
