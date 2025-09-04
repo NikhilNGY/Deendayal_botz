28d17d7663359ebcf126e12716fc3e5fc15ade8f
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-05 00:26:23 +0530

    Update gfilters.py

--- plugins/gfilters.py
+++ plugins/gfilters.py
@@ -9,7 +9,7 @@ from database.gfilters_mdb import(
 )
 from database.connections_mdb import active_connection
 from utils import get_file_id, gfilterparser, split_quotes
-from info import ADMINS
+from Deendayal_botz.info import ADMINS
 
 
 @Client.on_message(filters.command(['gfilter', 'addg']) & filters.incoming & filters.user(ADMINS))
