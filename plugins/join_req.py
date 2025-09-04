cae88dd121e3d59f075f9dede9695978037a82f5
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-05 00:25:14 +0530

    Update join_req.py

--- plugins/join_req.py
+++ plugins/join_req.py
@@ -3,7 +3,7 @@
 from pyrogram import Client, filters, enums
 from pyrogram.types import ChatJoinRequest
 from database.users_chats_db import db
-from info import ADMINS, AUTH_CHANNEL
+from Deendayal_botz.info import ADMINS, AUTH_CHANNEL
 
 @Client.on_chat_join_request(filters.chat(AUTH_CHANNEL))
 async def join_reqs(client, message: ChatJoinRequest):
