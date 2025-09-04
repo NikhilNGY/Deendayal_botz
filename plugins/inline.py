a542ca8ffb6df25e74895b743dbd9c31ad5a3e7f
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-04 16:30:25 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -35,7 +35,7 @@ async def answer(bot, query):
     if AUTH_CHANNEL and not await is_req_subscribed(bot, query):
         await query.answer(results=[],
                            cache_time=0,
-                           switch_pm_text='You have to subscribe my channel to use the bot',
+                           switch_pm_text=' ನಮಸ್ಕಾರ 🙏  ,\n \nಚಲನಚಿತ್ರವನ್ನು ಪಡೆಯಲು "JOIN CHANNEL" ಬಟನ್ ಕ್ಲಿಕ್ ಮಾಡಿ ಮತ್ತು ಚಾನಲ್‌ನಲ್ಲಿ ಸೇರಿಕೊಳ್ಳಿ.\n \n────── • ◆ • ──────\n \nYou Need to Join My Channel to Receive the Movie file. CLICK 👇👇',
                            switch_pm_parameter="subscribe")
         return
 
