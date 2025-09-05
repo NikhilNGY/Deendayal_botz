b4929e5dca387997ca5196843587300665821015
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-05 23:03:55 +0530

    Update file_properties.py

--- Deendayal_botz/util/file_properties.py
+++ Deendayal_botz/util/file_properties.py
@@ -4,7 +4,7 @@ from typing import Any, Optional
 from pyrogram.types import Message
 from pyrogram.file_id import FileId
 from pyrogram.raw.types.messages import Messages
-from Deendayal_botz.server.exceptions import FIleNotFound
+from Deendayal_botz.server.exceptions import FileNotFound
 
 
 async def parse_file_id(message: "Message") -> Optional[FileId]:
