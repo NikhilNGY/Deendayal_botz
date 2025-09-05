0711eaea4d268b6f61c6cfbd36cadf1f02646eb4
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-05 21:29:42 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -7,10 +7,10 @@ from pyrogram.types import (
     InlineQueryResultCachedDocument,
     InlineQuery,
 )
-from database.ia_filterdb import get_search_results
+from Deendayal_botz.database.ia_filterdb import get_search_results
 from utils import is_req_subscribed, get_size, temp
 from Deendayal_botz.info import CACHE_TIME, AUTH_USERS, AUTH_CHANNEL, CUSTOM_FILE_CAPTION
-from database.connections_mdb import active_connection
+from Deendayal_botz.database.connections_mdb import active_connection
 
 logger = logging.getLogger(__name__)
 cache_time = 0 if AUTH_USERS or AUTH_CHANNEL else CACHE_TIME
