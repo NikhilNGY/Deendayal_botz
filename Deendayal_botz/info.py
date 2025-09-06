be67b20898248a0e7682f7a0f18057a42818e6a9
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-06 17:42:21 +0530

    Update info.py

--- Deendayal_botz/info.py
+++ Deendayal_botz/info.py
@@ -1,6 +1,6 @@
 import re
 from os import environ,getenv
-from script import script 
+from Deendayal_botz.script import script 
 
 id_pattern = re.compile(r'^.\d+$')
 def is_enabled(value, default):
