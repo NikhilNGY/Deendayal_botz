b8f3d730bda1131425c8cf77d8a0b9643b53db93
NIKHIL <65222469+NikhilNGY@users.noreply.github.com>
2025-09-06 18:53:46 +0530

    Update info.py

--- Deendayal_botz/info.py
+++ Deendayal_botz/info.py
@@ -50,11 +50,12 @@ BIN_CHANNEL = int(environ.get('BIN_CHANNEL', '-1001693006436'))  # Bin channel i
 PREMIUM_LOGS = int(environ.get('PREMIUM_LOGS', '-1001693006436'))  # Premium logs channel id
 DELETE_CHANNELS = [int(dch) if id_pattern.search(dch) else dch for dch in environ.get('DELETE_CHANNELS', '-1001396923650').split()] #(make sure bot is admin)
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002241869735')  # Support group id (make sure bot is admin)
+DEENDAYAL_MOVIE_UPDATE_CHANNEL = environ.get("DEENDAYAL_MOVIE_UPDATE_CHANNEL", "-1001951277428")
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1002071201127')  # Request channel id (make sure bot is admin)
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/+sGC3kK3Q9L1kNDNl')  # Support group link (make sure bot is admin)
 
 # FORCE_SUB 
-auth_req_channels = environ.get("AUTH_REQ_CHANNELS", "-100")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
+auth_req_channels = environ.get("AUTH_REQ_CHANNELS", " ")# requst to join Channel for force sub (make sure bot is admin) only for bot ADMINS  
 auth_channels     = environ.get("AUTH_CHANNELS", "-1001951277428")# Channels for force sub (make sure bot is admin)
 
 # ============================
